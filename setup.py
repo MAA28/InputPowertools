dd4f24a195beb2fd81bfc095ed9944515c4bb89d
MA28 <malteaschenbach@gmail.com>
2022-03-19 19:25:39 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='InputPowertools',
-    version='0.1',
+    version='0.2',
     description='Kind of like a non intrusive addon for the standard input()',
     long_description=long_description,
     long_description_content_type='text/markdown',
